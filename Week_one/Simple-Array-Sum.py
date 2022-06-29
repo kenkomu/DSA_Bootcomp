f0331cce953321f5a3f1e6697f0db73b241bb24b
kenkomu <komukenneth216@gmail.com>
2022-06-29 09:54:18 +0300

    Week One

---
+++ Week_one/Simple-Array-Sum.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    ar = [1,2,3,4,10,11]
+    n = len(ar)
+    simpleArraySum = 0
+    for n in ar:
+        simpleArraySum = simpleArraySum+n
+    return(simpleArraySum)
+
+    print("Sum of array",sum)
+    print(n)
+
+
